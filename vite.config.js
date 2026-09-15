b0484a75f79cce831901aafa43e1b8140bc9e752
Yogesh <yogeshbattula55@gmail.com>
2026-09-15 17:30:07 +0530

    Configure custom port and ESM type in vite config

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+    strictPort: false,
+    allowedHosts: true,
+    cors: true,
+  },
+  preview: {
+    host: true,
+    port: 3000,
+    strictPort: false,
+    allowedHosts: true,
+    cors: true,
+  }
+});
