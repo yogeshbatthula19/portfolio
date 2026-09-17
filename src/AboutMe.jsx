ba29ddb9397fa1ed88469f3090537b36326862f4
Yogesh <yogeshbattula55@gmail.com>
2026-09-17 12:48:04 +0530

    Make hand-drawn SVG arrows accurate in shape and placement matching reference photo

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -12,9 +12,8 @@ export default function AboutMe({ onContact, onProjects }) {
 
   return (
     <div className="polaroid-editorial-screen">
-
       {/* =================================================================
-          MAIN EDITORIAL STAGE (Polaroid + Annotations + Hand-Drawn Arrows)
+          MAIN EDITORIAL STAGE (Polaroid + Hand-Drawn Annotations & Arrows)
           ================================================================= */}
       <div className="editorial-stage">
         
@@ -25,35 +24,49 @@ export default function AboutMe({ onContact, onProjects }) {
           </div>
         </div>
 
-        {/* 1. Top Center Note & Upward Curved Arrow */}
+        {/* 1. Top Center: Role */}
         <div className="editorial-note note-top-center">
           <span className="note-label">ROLE</span>
           <span className="note-script">Product & Systems</span>
         </div>
-        <svg className="arrow-top-center hand-drawn-arrow" viewBox="0 0 44 55" fill="none">
-          <path d="M 6 48 C 18 42, 28 28, 24 10" />
-          <path d="M 18 16 L 24 9 L 30 15" />
-        </svg>
 
-        {/* 2. Top Left Note */}
+        {/* 2. Top Left: Nickname */}
         <div className="editorial-note note-top-left">
           <span className="note-label">nickname:</span>
           <span className="note-script">the pixel architect</span>
         </div>
 
-        {/* 3. Top Right Note */}
+        {/* 3. Top Right: Multidisciplinary + Arrow 1 (Sweeping Up-Right) */}
         <div className="editorial-note note-top-right">
           <span className="note-script">multidisciplinary</span>
         </div>
+        <svg
+          className="arrow-top-right hand-drawn-arrow"
+          viewBox="0 0 32 54"
+          fill="none"
+          aria-hidden="true"
+        >
+          {/* Sweeping curve from polaroid corner up-right */}
+          <path d="M 4 48 C 16 38, 22 22, 20 8" />
+          {/* Hand-drawn open arrowhead pointing up-right */}
+          <path d="M 12 16 L 20 7 L 26 17" />
+        </svg>
 
-        {/* 4. Middle Left Note & Squiggly Arrow to Photo */}
+        {/* 4. Middle Left: Favorite Movies + Arrow 2 (Serpentine Wavy Doodle) */}
         <div className="editorial-note note-mid-left">
           <span className="note-label">favorite movies:</span>
           <span className="note-script">sci-fi & crime</span>
         </div>
-        <svg className="arrow-mid-left hand-drawn-arrow" viewBox="0 0 32 48" fill="none">
-          <path d="M 2 40 Q 14 36, 6 24 T 22 10" />
-          <path d="M 14 10 L 23 9 L 23 18" />
+        <svg
+          className="arrow-mid-left hand-drawn-arrow"
+          viewBox="0 0 30 62"
+          fill="none"
+          aria-hidden="true"
+        >
+          {/* Authentic 2-wave serpentine squiggle snaking upwards */}
+          <path d="M 10 56 C 2 46, 0 38, 9 30 C 18 22, 8 15, 14 7" />
+          {/* Hand-drawn open arrowhead pointing up-right into the photo */}
+          <path d="M 7 14 L 15 6 L 22 13" />
         </svg>
 
         {/* 5. Center Hero Polaroid Frame */}
@@ -82,23 +95,30 @@ export default function AboutMe({ onContact, onProjects }) {
           </button>
         </article>
 
-        {/* 6. Middle Right Note */}
+        {/* 6. Middle Right: Superpower */}
         <div className="editorial-note note-mid-right">
           <span className="note-label">superpower:</span>
           <span className="note-script">craft & micro-details</span>
         </div>
 
-        {/* 7. Bottom Left Note (Overlapping peeking card) */}
+        {/* 7. Bottom Left: Passions outside design */}
         <div className="editorial-note note-bottom-left">
           <span className="note-label">passions outside design:</span>
           <span className="note-script">cars & tech gadgets</span>
           <span className="note-subscript">(and dark roast coffee)</span>
         </div>
 
-        {/* 8. Bottom Right Note & Curved Downward Arrow */}
-        <svg className="arrow-bottom-right hand-drawn-arrow" viewBox="0 0 38 38" fill="none">
-          <path d="M 4 6 Q 16 12, 22 28" />
-          <path d="M 14 26 L 23 29 L 26 20" />
+        {/* 8. Bottom Right: Arrow 3 (Hooked Arc) + Favorite Tools */}
+        <svg
+          className="arrow-bottom-right hand-drawn-arrow"
+          viewBox="0 0 34 34"
+          fill="none"
+          aria-hidden="true"
+        >
+          {/* Hooked curve swooping down-right from polaroid chin */}
+          <path d="M 5 5 C 4 16, 11 25, 23 29" />
+          {/* Hand-drawn open arrowhead pointing down-right towards tools */}
+          <path d="M 15 29 L 24 30 L 24 21" />
         </svg>
         <div className="editorial-note note-bottom-right">
           <span className="note-label">favorite tools:</span>
