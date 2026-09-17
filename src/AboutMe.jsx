e25166bb367514fd1a107338293abb046cf15764
Yogesh <yogeshbattula55@gmail.com>
2026-09-17 12:53:55 +0530

    Add realistic die-cut stickers for Charminar, Drone, and Jeep around the scrapbook

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -13,7 +13,7 @@ export default function AboutMe({ onContact, onProjects }) {
   return (
     <div className="polaroid-editorial-screen">
       {/* =================================================================
-          MAIN EDITORIAL STAGE (Polaroid + Hand-Drawn Annotations & Arrows)
+          MAIN EDITORIAL STAGE (Polaroid + Hand-Drawn Annotations & Stickers)
           ================================================================= */}
       <div className="editorial-stage">
         
@@ -24,19 +24,49 @@ export default function AboutMe({ onContact, onProjects }) {
           </div>
         </div>
 
+        {/* ===============================================================
+            REALISTIC DIE-CUT VINYL STICKERS (DRONE, CHARMINAR, JEEP)
+            =============================================================== */}
+        {/* Drone Sticker on top-left */}
+        <div
+          className="realistic-sticker sticker-drone"
+          title="Tech & FPV Drone"
+          aria-label="Quadcopter camera drone sticker"
+        >
+          <img src="/images/drone-sticker.png" alt="Camera drone die-cut sticker" />
+        </div>
+
+        {/* Charminar Hyderabad Monument Sticker on top-right */}
+        <div
+          className="realistic-sticker sticker-charminar"
+          title="Charminar, Hyderabad"
+          aria-label="Charminar Hyderabad monument sticker"
+        >
+          <img src="/images/charminar-sticker.png" alt="Charminar monument die-cut sticker" />
+        </div>
+
+        {/* Off-road 4x4 Jeep Sticker on bottom-left */}
+        <div
+          className="realistic-sticker sticker-jeep"
+          title="4x4 Adventure Jeep"
+          aria-label="Off-road 4x4 Jeep sticker"
+        >
+          <img src="/images/jeep-sticker.png" alt="Jeep 4x4 die-cut sticker" />
+        </div>
+
         {/* 1. Top Center: Role */}
         <div className="editorial-note note-top-center">
           <span className="note-label">ROLE</span>
           <span className="note-script">Product & Systems</span>
         </div>
 
-        {/* 2. Top Left: Nickname */}
+        {/* 2. Top Left: Nickname (below drone sticker) */}
         <div className="editorial-note note-top-left">
           <span className="note-label">nickname:</span>
           <span className="note-script">the pixel architect</span>
         </div>
 
-        {/* 3. Top Right: Multidisciplinary + Arrow 1 (Sweeping Up-Right) */}
+        {/* 3. Top Right: Multidisciplinary + Arrow 1 */}
         <div className="editorial-note note-top-right">
           <span className="note-script">multidisciplinary</span>
         </div>
@@ -46,13 +76,11 @@ export default function AboutMe({ onContact, onProjects }) {
           fill="none"
           aria-hidden="true"
         >
-          {/* Sweeping curve from polaroid corner up-right */}
           <path d="M 4 48 C 16 38, 22 22, 20 8" />
-          {/* Hand-drawn open arrowhead pointing up-right */}
           <path d="M 12 16 L 20 7 L 26 17" />
         </svg>
 
-        {/* 4. Middle Left: Favorite Movies + Arrow 2 (Serpentine Wavy Doodle) */}
+        {/* 4. Middle Left: Favorite Movies + Arrow 2 */}
         <div className="editorial-note note-mid-left">
           <span className="note-label">favorite movies:</span>
           <span className="note-script">sci-fi & crime</span>
@@ -63,9 +91,7 @@ export default function AboutMe({ onContact, onProjects }) {
           fill="none"
           aria-hidden="true"
         >
-          {/* Authentic 2-wave serpentine squiggle snaking upwards */}
           <path d="M 10 56 C 2 46, 0 38, 9 30 C 18 22, 8 15, 14 7" />
-          {/* Hand-drawn open arrowhead pointing up-right into the photo */}
           <path d="M 7 14 L 15 6 L 22 13" />
         </svg>
 
@@ -101,23 +127,21 @@ export default function AboutMe({ onContact, onProjects }) {
           <span className="note-script">craft & micro-details</span>
         </div>
 
-        {/* 7. Bottom Left: Passions outside design */}
+        {/* 7. Bottom Left: Passions outside design (paired with Jeep sticker) */}
         <div className="editorial-note note-bottom-left">
           <span className="note-label">passions outside design:</span>
           <span className="note-script">cars & tech gadgets</span>
           <span className="note-subscript">(and dark roast coffee)</span>
         </div>
 
-        {/* 8. Bottom Right: Arrow 3 (Hooked Arc) + Favorite Tools */}
+        {/* 8. Bottom Right: Arrow 3 + Favorite Tools */}
         <svg
           className="arrow-bottom-right hand-drawn-arrow"
           viewBox="0 0 34 34"
           fill="none"
           aria-hidden="true"
         >
-          {/* Hooked curve swooping down-right from polaroid chin */}
           <path d="M 5 5 C 4 16, 11 25, 23 29" />
-          {/* Hand-drawn open arrowhead pointing down-right towards tools */}
           <path d="M 15 29 L 24 30 L 24 21" />
         </svg>
         <div className="editorial-note note-bottom-right">
